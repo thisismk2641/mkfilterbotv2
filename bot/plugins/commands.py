787df68be82686b8c7f47da0e61322d5f41ee470
thisismk2641 <85360630+thisismk2641@users.noreply.github.com>
2021-07-13 19:18:51 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -90,10 +90,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
+        InlineKeyboardButton('Developers', url='https://t.me/Thisis_mk'),
         InlineKeyboardButton('Source Code 🧾', url ='https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot-V2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/Thisis_mk')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
